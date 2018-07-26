59553955f1ebbda6ddafed20a5e79be32fa7a18c
Munoz Mendez <Fernando.MunozMendez@aecom.com>
2018-07-26 10:00:03 +0100

    Added some comments and removing the requirements to write and read

--- R/read_ufm.R
+++ R/read_ufm.R
@@ -1,5 +1,18 @@
+#' Read UFM function
+#'
+#' This function allows you to express your love of cats.
+#' @param file The file to read.
+#' @param MX Path to the MX exe folder
+#' @param remove_txt when set to TRUE it will remove the interim txt files
+#' @param clean_up when TRUE removes the VDU, KEY and LPX files
+#' @keywords read, ufm
+#' @export
+#' @examples
+#' read_ufm("file.UFM")
+
+
 read_ufm <- function(file ,MX = "C:\\SATWIN\\XEXES_11.3.12W_MC\\$MX.exe", remove_txt = TRUE, clean_up = TRUE){
-# Read data frame into UFM (TUBA 2 ONLY) 
+# Read data frame into UFM (TUBA 2 ONLY)
 #   x - data frame with 3 columns (O,D, Trips)
 #   file -  name of the file with or without extension
 #   MX - path to MX folder default value included
@@ -7,20 +20,20 @@ read_ufm <- function(file ,MX = "C:\\SATWIN\\XEXES_11.3.12W_MC\\$MX.exe", remove
 #   clean_up - if true removes the VDU, LPX and KEY files
 # Output
 #   Writes a UFM file into the parent folder
-# Bugs  
+# Bugs
 #   It does not clean up in other folders if you set up a relative path in file
   #file <- paste0(dirname(file),"/",basename(file),".UFM")
-  require(data.table)
-  require(readr)
-  require(stringr)
+  # require(data.table)
+  # require(readr)
+  # require(stringr)
   #name <- paste0(file,".txt") # Name of the interim file
-  
+
   #fwrite(x,file = name, col.names = F) # Write the interim file
-  
-  
+
+
   keyfile <- "temp.key" #name of the key
 
-# Text on the key  
+# Text on the key
 text <-"          13                                                                2000
           16                                                                2604
            9                                                                2604
@@ -30,25 +43,25 @@ text <-"          13                                                            
 y                                                                           9200"
 
 # Write the keyfile
-  write_lines(text,keyfile)
+  readr::write_lines(text,keyfile)
 # Execute the command
 
-  command <- paste(MX, 
+  command <- paste(MX,
                    paste0("'",file,"'"), # Added commas
                    "KEY temp.key VDU vdu")
-  
+
   system(command)
-  
+
 # Read the csv in R
-  txtfile <- paste0(dirname(file),"/",str_sub(basename(file),0,-4),"TXT")
-  matriz<-  fread(txtfile)
+  txtfile <- paste0(dirname(file),"/",stringr::str_sub(basename(file),0,-4),"TXT")
+  matriz<-  data.table::fread(txtfile)
   names(matriz) <- c("Origin", "Destination", "User_class", "Trips")
-  
-  # Remove interim file 
+
+  # Remove interim file
   if (remove_txt) {
     file.remove(txtfile)
   }
-  
+
   #Clean up garbage files
   if (clean_up) {
     file.remove(list.files(pattern = "*.VDU"))
@@ -57,7 +70,3 @@ y                                                                           9200
   }
   return(matriz)
 }
-
-df <- read_ufm("C:\\Projects\\A428 BC2CG\\Stage 3\\TIS - Factored\\Matrices\\TIS_Matrix_3UC_TS1.UFM")
-
-file <- "C:\\Projects\\A428 BC2CG\\Stage 3\\TIS - Factored\\Matrices\\TIS_Matrix_3UC_TS1.UFM"
--- R/write_ufm.R
+++ R/write_ufm.R
@@ -1,5 +1,18 @@
+#' Read UFM function
+#'
+#' This function allows you to express your love of cats.
+#' @param x A data,table object with three columns O,D,Trips.
+#' @param file The file to write, extension mandatory.
+#' @param MX Path to the MX exe folder
+#' @param remove_txt when set to TRUE it will remove the interim txt files
+#' @param clean_up when TRUE removes the VDU, KEY and LPX files
+#' @keywords write, ufm
+#' @export
+#' @examples
+#' read_ufm("file.UFM")
+
 write_ufm <- function(x, file ,MX = "C:\\SATWIN\\XEXES_11.3.12W_MC\\$MX.exe", remove_txt = TRUE, clean_up = TRUE){
-# Writes data frame into UFM (TUBA 2 ONLY) 
+# Writes data frame into UFM (TUBA 2 ONLY)
 #   x - data frame with 3 columns (O,D, Trips)
 #   file -  name of the file with extension, paths accepted
 #   MX - path to MX folder default value included
@@ -7,29 +20,29 @@ write_ufm <- function(x, file ,MX = "C:\\SATWIN\\XEXES_11.3.12W_MC\\$MX.exe", re
 #   clean_up - if true removes the VDU, LPX and KEY files
 # Output
 #   Writes a UFM file into the parent folder
-# Bugs  
+# Bugs
 #   It does not clean up in other folders if you set up a relative path in file
 
-  
-  require(data.table)
-  require(stringr)
-  require(readr)
-  
-  if (str_sub(file,-5,-1) != ".UFM" | str_sub(file,-5,-1) != ".ufm") {
+
+  # require(data.table)
+  # require(stringr)
+  # require(readr)
+
+  if (str_sub(file,-5,-1) != ".UFM" | stringr::str_sub(file,-5,-1) != ".ufm") {
     warning("File should have an appropiate extension like .UFM or .ufm")
   }
-  
-  
+
+
   txtName <- paste0(dirname(file),"/",
-                 str_sub(basename(file),0,-5),
+                 stringr::str_sub(basename(file),0,-5),
                  ".txt") # Name of the interim file
-  
-  fwrite(x,file = txtName, col.names = F) # Write the interim file
-  
-  
+
+  data.table::fwrite(x,file = txtName, col.names = F) # Write the interim file
+
+
   keyfile <- "temp.key" #name of the key
 
-# Text on the key  
+# Text on the key
 text <-c(
 "           1                                                                2004",
 txtName,
@@ -46,16 +59,16 @@ y                                                                           9200
 ")
 
 # Write the keyfile
-  write_lines(text,keyfile)
+  readr::write_lines(text,keyfile)
 # Execute the command
   command <- paste(MX, "I KEY temp.key VDU vdu")
   system(command)
 
-  # Remove interim file 
+  # Remove interim file
   if (remove_txt) {
     file.remove(txtName)
   }
-  
+
   #Clean up garbage files
   if (clean_up) {
     file.remove(list.files(pattern = "*.VDU"))
