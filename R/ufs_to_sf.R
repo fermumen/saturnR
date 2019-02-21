e7c6d57543232a9e73f7f09f12a9f1a238a6ed20
Fernando Munoz Mendez <fermm.92@gmail.com>
2019-02-21 11:58:28 +0000

    New manual and version number

--- R/ufs_to_sf.R
+++ R/ufs_to_sf.R
@@ -3,8 +3,8 @@
 #' This function allows you to read UFS/N file's into an R SF.
 #' @param file The file to read.
 #' @param SATDB Path to the SATDB exe folder
-#' @param lat_long when set to TRUE it will convert coordinates to
 #' @param clean_up when TRUE removes the VDU, KEY and LPX files
+#' @param remove_txt when TRUE removes the interim txt
 #' @keywords read, ufs, coordinates
 #' @export
 #' @examples
